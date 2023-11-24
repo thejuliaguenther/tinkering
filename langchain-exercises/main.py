a25b120e9d31b83ef5078267fb31f2c49241e4fe
Julia Guenther <juliaguenther@Julias-MacBook-Air-802.local>
2023-11-24 16:58:24 -0500

    Added second chain

--- langchain-exercises/main.py
+++ langchain-exercises/main.py
@@ -1,7 +1,7 @@
 # import os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -20,14 +20,36 @@ code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}"
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code:\n{code}"
 )
 
-result = code_chain({
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+	chains=[code_chain,test_chain],
+	input_variables=["task", "language"],
+	output_variables=["test","code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result["text"])
+print(">>>>>> GENERATED CODE:")
+print(result["code"])
+
+print(">>>>>> GENERATED TEST:")
+print(result["test"])
