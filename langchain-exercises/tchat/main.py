78755b1d2287eb2b1f01cd5f8d647587933fd517
Julia Guenther <juliag1@alumni.stanford.edu>
2023-11-25 14:46:48 -0500

    Added save to file

--- langchain-exercises/tchat/main.py
+++ langchain-exercises/tchat/main.py
@@ -3,13 +3,18 @@
 from langchain.chat_models import ChatOpenAI
 from langchain import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True
+)
+
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
     messages=[
