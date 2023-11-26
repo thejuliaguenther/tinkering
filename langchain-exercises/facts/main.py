bb482646f66a6673687b4af88ee309029911e424
Julia Guenther <juliag1@alumni.stanford.edu>
2023-11-26 12:30:44 -0500

    Added chunking

--- langchain-exercises/facts/main.py
+++ langchain-exercises/facts/main.py
@@ -1,9 +1,20 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+    )
 
-print(docs)
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+    )
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
