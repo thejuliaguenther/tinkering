a42c6045e5fb4c143c4f80a7556302e2989cf6bf
Julia Guenther <juliag1@alumni.stanford.edu>
2023-11-26 13:19:26 -0500

    Addded ChromaDB functionality

--- langchain-exercises/facts/main.py
+++ langchain-exercises/facts/main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,13 +14,24 @@ text_splitter = CharacterTextSplitter(
     separator="\n",
     chunk_size=200,
     chunk_overlap=0
-    )
+)
 
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(
     text_splitter=text_splitter
-    )
+)
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search(
+    "What is an interesting fact about the English language?"
+)
+
+for result in results:
     print("\n")
+    print(result.page_content)
+    
